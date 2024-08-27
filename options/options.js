ce1431c60dcbcf6bad093f851761b7064c9bb5bc
Crowdbotics <team@crowdbotics.com>
2024-08-27 03:19:31 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "the-colorado-experi-49172",
+  url: "https://the-colorado-experi-49172.botics.co",
+  api: "https://the-colorado-experi-49172.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
